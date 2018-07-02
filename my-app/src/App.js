209fc66d384213e607a69290e6d39d0d23b2170d
sethusuresh <sethusuresh.ekm@gmail.com>
2018-07-03 00:35:03 +0530

    demo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from "./Person/Person"
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         <p> 
           By {this.props.name}
         </p>
+        <Person />
       </div>
     );
   }
